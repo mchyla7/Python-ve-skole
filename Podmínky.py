54d05969e4107939053acd8d18ec6784e3155d85
m_chyla <mchyla7@gmail.com>
2022-10-12 10:46:17 +0200

    Testinggg github

--- Podmínky.py
+++ Podmínky.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 #*  Relační podmínky
 # x == y                # x Je rovno y 
@@ -58,33 +57,3 @@ def absolutnihodnota():
     else:
         print("Číslo je vzdáleno od nuly o: ", cislo)
 absolutnihodnota()
-=======
-
-#*  Relační podmínky
-# x == y                # x Je rovno y 
-# x != y                # x není rovno y 
-# x > y                 # x je větší než y
-# x < y                 # x je menší než y
-# x <= y                # x je větší nebo rovno y
-# x >= y                # x je mneší nebo rovno y
-
-
-#Zjišťujeme jestli je číslo kladné
-
-def kladne():
-    cislo = float(input("Číslo: "))
-    if cislo>0:
-        print("Číslo je kladné ✔")
-#kladne()
-
-# Zjišťujeme jestli je číslo kladné nebo nekladné
-
-
-def kladne_nekladne():
-    cislo = float(input("Číslo: "))
-    if cislo>0:
-        print("Číslo je kladné ✔")
-    else:
-        print("Číslo není kladné ❌")
-kladne_nekladne()
->>>>>>> 0a66787e8ff9e4f0bc0c5f0731d90549b5c127f2

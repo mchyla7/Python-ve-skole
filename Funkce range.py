4f33ac37da48aac831443683f5e33a92988c91f0
m_chyla <mchyla7@gmail.com>
2022-11-30 11:18:53 +0100

    ➕ Přidán první program a vysvětlení funkce range

---
+++ Funkce range.py
@@ -0,0 +1,10 @@
+# Program zobrazí čísla od 1 do 10
+
+# funkce range(hodnota) - vygeneruje rozsah hodnot
+# funkce range(10) - vygeneruje rozsah čísel od 1ky do 10ky
+# funkce range(-10,10) - vygeneruje rozsah celých čísel od -10ky do 9ky
+
+def program():
+    for i in range(10):
+        print(i)
+program()
